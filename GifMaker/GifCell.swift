5cdd350c00e258e5610c7d5548fca6f9fbd02ff2
Abad Vera <abad_vera@me.com>
2016-11-17 14:30:24 -0800

    Added a Select function on the first page. This will enable the user to delete gifs (already implemented) and also edit a gif so that it has a different title (not yet implemented

--- GifMaker/GifCell.swift
+++ GifMaker/GifCell.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class GifCell: UICollectionViewCell {
     
+    @IBOutlet weak var checkImage: UIImageView!
     @IBOutlet weak var gifImageView: UIImageView!
+    var prevSelection: Bool!
+    var storedGif: Gif!
     
     func configureForGif(gif: Gif) {
+        storedGif = gif
+        checkImage.isHidden = true
         gifImageView.image = gif.gifImage
+        isSelected = false
+    }
+    
+    func selectCell() {
+        checkImage.isHidden = !isSelected
+        gifImageView.isOpaque = isSelected
     }
 }
--- GifMaker/SavedGifsViewController.swift
+++ GifMaker/SavedGifsViewController.swift
@@ -20,19 +20,34 @@ class SavedGifsViewController: UIViewController, UICollectionViewDelegate, UICol
     
     var savedGifs: [Gif]? = [Gif]()
     let cellMargin = CGFloat(12.0)
+    var isSelectionState = false
+    
+    // UIBarButtonItems
+    var deleteBttnItem: UIBarButtonItem!
+    var editBttnItem: UIBarButtonItem!
+    var rightBarItem:UIBarButtonItem!
     
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var emptyView: UIStackView!
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
+        collectionView.reloadData()
+        self.applyTheme(theme: .Light)
         // Hide if there are no savedGifs
         if let count = savedGifs?.count {
             emptyView.isHidden = count > 0 ? true : false
             self.navigationController?.navigationBar.isHidden = count > 0 ? false : true
+            if count > 0 {
+                rightBarItem = UIBarButtonItem(title: "Select", style: .plain, target: self, action: #selector(enableSelectState))
+                self.navigationItem.rightBarButtonItem = rightBarItem
+                // init other barButtonItems
+                editBttnItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(editGif))
+                deleteBttnItem = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(deleteGif))
+            }
         }
-        collectionView.reloadData()
-        self.applyTheme(theme: .Light)
+        
     }
     
     override func viewDidLoad() {
@@ -74,11 +89,88 @@ class SavedGifsViewController: UIViewController, UICollectionViewDelegate, UICol
     func previewVC(preview: PreviewViewController, didSaveGif gif: Gif) {
         gif.gifData = NSData(contentsOf: gif.url as! URL)
         savedGifs?.append(gif)
-        print("Gif count \(savedGifs?.count)")
-        NSKeyedArchiver.archiveRootObject(savedGifs!, toFile: gifsFilePath)
+        // NSKeyedArchiver.archiveRootObject(savedGifs!, toFile: gifsFilePath)
+        saveGifs(gifs: savedGifs!)
+    }
+    
+    func saveGifs(gifs: [Gif]) {
+        NSKeyedArchiver.archiveRootObject(gifs, toFile: gifsFilePath)
     }
     
     // MARK: CollectionView Delegate and DataSource methods
+    
+    func enableSelectState() {
+        // Add delete and edit barButtonItems to navBar
+        if isSelectionState {
+            self.navigationItem.leftBarButtonItems?.removeAll()
+            rightBarItem.title = "Select"
+            cancelSelectedCells(paths: collectionView.indexPathsForSelectedItems)
+            isSelectionState = !isSelectionState
+            collectionView.allowsMultipleSelection = !isSelectionState
+        } else {
+            collectionView.allowsMultipleSelection = !isSelectionState
+            editBttnItem.isEnabled = false
+            deleteBttnItem.isEnabled = false
+            rightBarItem.title = "Done"
+            self.navigationItem.leftBarButtonItems = [editBttnItem, deleteBttnItem]
+            isSelectionState = !isSelectionState
+        }
+    }
+    
+    // Enable left bar button items when selection is above 0
+    // but only editBttnItem can be chosen if selected items is 1 and only 1
+    func enableBttnItems(cellCount: Int?) {
+        guard let count = cellCount else {
+            return
+        }
+        
+        if count > 1 {
+            editBttnItem.isEnabled = false
+            deleteBttnItem.isEnabled = true
+            
+        }
+        if count == 1 {
+            editBttnItem.isEnabled = true
+            deleteBttnItem.isEnabled = true
+        }
+        if count == 0 {
+            editBttnItem.isEnabled = false
+            deleteBttnItem.isEnabled = false
+        }
+    }
+    
+    // If selection state is canceled then remove all selected items
+    func cancelSelectedCells(paths: [IndexPath]?) {
+        guard let indexP = paths else {
+            return
+        }
+        for path in indexP {
+            collectionView.deselectItem(at: path, animated: true)
+            let cell = getCell(path: path)
+            cell.selectCell()
+        }
+    }
+    
+    func editGif() {
+        print("Edit function called")
+    }
+    
+    // Delete selected gifs
+    func deleteGif() {
+        let cellsIndexPaths = collectionView.indexPathsForSelectedItems
+        for indexP in cellsIndexPaths! {
+            let cellToDel = getCell(path: indexP)
+            savedGifs = savedGifs?.filter() {$0 != cellToDel.storedGif}
+        }
+        // First erase stored array then save the modified array
+        do {
+            try FileManager.default.removeItem(atPath: gifsFilePath)
+        } catch {
+            print("Error erasing item: \(error)")
+        }
+        saveGifs(gifs: savedGifs!)
+        collectionView.reloadData() // Reload collection view to show removed items
+    }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         if let count = savedGifs?.count {
@@ -97,11 +189,37 @@ class SavedGifsViewController: UIViewController, UICollectionViewDelegate, UICol
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let gif = savedGifs?[indexPath.item]
-        let detailVC = self.storyboard?.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
-        
-        detailVC.gif = gif
-        detailVC.modalPresentationStyle = .overCurrentContext
-        present(detailVC, animated: true, completion: nil)
+        // Check to see if in selection state or not
+        if isSelectionState {
+            enableBttnItems(cellCount: collectionView.indexPathsForSelectedItems?.count)
+            let cell = getCell(path: indexPath)
+            cell.selectCell()
+            
+        } else {
+            let detailVC = self.storyboard?.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
+            
+            detailVC.gif = gif
+            detailVC.modalPresentationStyle = .overCurrentContext
+            present(detailVC, animated: true, completion: nil)
+        }
+    }
+    
+    func getCell(path: IndexPath) -> GifCell {
+        let cell = collectionView.cellForItem(at: path) as! GifCell
+        return cell
+    }
+    
+    // Only deselect cells when in the selection state
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        if isSelectionState {
+            enableBttnItems(cellCount: collectionView.indexPathsForSelectedItems?.count)
+            let cell = getCell(path: indexPath)
+            cell.selectCell()
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
     }
     
     // MARK: CollectionViewFlowLayout
